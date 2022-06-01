7a5e927b57dc40cf106287f7228643dad42174ec
Bob McWhirter <bmcwhirt@redhat.com>
2022-06-01 13:57:13 -0400

    Add some tests around Label UUID and VirtualAddress.

--- btmesh-common/src/address/virtual_address.rs
+++ btmesh-common/src/address/virtual_address.rs
@@ -3,6 +3,7 @@ use crate::{crypto, ParseError};
 use cmac::crypto_mac::InvalidKeyLength;
 use core::convert::TryInto;
 
+/// A virtual address representing possibly several unique label UUIDs.
 #[derive(Copy, Clone, PartialEq, Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct VirtualAddress(u16);
@@ -51,6 +52,7 @@ impl Into<Address> for VirtualAddress {
     }
 }
 
+/// A unique label UUID used for virtual addresses to address multiple destinations.
 #[derive(Copy, Clone, PartialEq, Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct LabelUuid {
@@ -86,6 +88,9 @@ impl defmt::Format for LabelUuid {
 }
 
 impl LabelUuid {
+    /// Parse a 16 octet label UUID.
+    ///
+    /// Returns a `ParseError` in the event the passed-in UUID is less than or longer than 16 octets.
     pub fn parse(uuid: &[u8]) -> Result<Self, ParseError> {
         if uuid.len() != 16 {
             Err(ParseError::InvalidLength)
@@ -119,5 +124,30 @@ impl LabelUuid {
         hash[0] = (0b00111111 & hash[0]) | 0b10000000;
         let hash = u16::from_be_bytes([hash[0], hash[1]]);
         Ok(VirtualAddress(hash))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::address::{LabelUuid, VirtualAddress};
+
+    // Virtual addr: 800f, label: a04bf881e4a7bf702dfee1638ab8b2b3
+    const UUID: [u8;16] = [0xa0, 0x4b, 0xf8, 0x81, 0xe4, 0xa7, 0xbf, 0x70, 0x2d, 0xfe, 0xe1, 0x63, 0x8a, 0xb8, 0xb2, 0xb3];
+    const ADDR: u16 = 0x800f;
+
+    #[test]
+    fn virtual_address() {
+        let label = LabelUuid::new( UUID );
+        unsafe {
+            assert_eq!(label.unwrap().virtual_address(), VirtualAddress::new_unchecked(ADDR))
+        }
+    }
+
+    #[test]
+    fn virtual_address_of() {
+        unsafe {
+            assert_eq!(LabelUuid::virtual_address_of(UUID),
+                       Ok(VirtualAddress::new_unchecked(ADDR)))
+        }
     }
 }
