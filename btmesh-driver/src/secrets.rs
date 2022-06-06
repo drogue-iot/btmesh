a83bf2a2b6b7eec0bc120dabc2a679ed404cc25f
Bob McWhirter <bmcwhirt@redhat.com>
2022-06-06 13:54:36 -0400

    Better testing around NetworkKey/NID derivation.

--- btmesh-driver/src/secrets.rs
+++ btmesh-driver/src/secrets.rs
@@ -1,5 +1,5 @@
 use crate::{Driver, DriverError, NetworkKeyHandle};
-use btmesh_common::Nid;
+use btmesh_common::{crypto, Nid};
 use core::iter::Filter;
 use core::slice::Iter;
 
@@ -72,6 +72,19 @@ pub(crate) struct NetworkKey {
     privacy_key: [u8; 16],
     encryption_key: [u8; 16],
     nid: Nid,
+}
+
+impl NetworkKey {
+    pub fn new(network_key: [u8; 16]) -> Result<Self, DriverError> {
+        let (nid, encryption_key, privacy_key) =
+            crypto::k2(&network_key, &[0x00]).map_err(|_| DriverError::CryptoError)?;
+
+        Ok(Self {
+            privacy_key,
+            encryption_key,
+            nid: Nid::new(nid),
+        })
+    }
 }
 
 pub(crate) struct NetworkKeyIter<'i, I: Iterator<Item = &'i Option<NetworkKey>>> {
@@ -176,5 +189,30 @@ mod tests {
         }
 
         assert_eq!(0, found);
+    }
+
+    #[test]
+    fn network_key_derivation() {
+        // 8.2.2 Encryption and privacy keys (Master)
+        let network_key: [u8; 16] = [
+            0x7d, 0xd7, 0x36, 0x4c, 0xd8, 0x42, 0xad, 0x18, 0xc1, 0x7c, 0x2b, 0x82, 0x0c, 0x84,
+            0xc3, 0xd6,
+        ];
+
+        let encryption_key: [u8; 16] = [
+            0x09, 0x53, 0xfa, 0x93, 0xe7, 0xca, 0xac, 0x96, 0x38, 0xf5, 0x88, 0x20, 0x22, 0x0a,
+            0x39, 0x8e,
+        ];
+
+        let privacy_key: [u8; 16] = [
+            0x8b, 0x84, 0xee, 0xde, 0xc1, 0x00, 0x06, 0x7d, 0x67, 0x09, 0x71, 0xdd, 0x2a, 0xa7,
+            0x00, 0xcf,
+        ];
+
+        let network_key = NetworkKey::new(network_key).unwrap();
+
+        assert_eq!(Nid::new(0x68), network_key.nid);
+        assert_eq!(privacy_key, network_key.privacy_key);
+        assert_eq!(encryption_key, network_key.encryption_key);
     }
 }
