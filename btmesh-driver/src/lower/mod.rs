33be89109e3bad7455bfa57eb119ed4060002a70
Bob McWhirter <bmcwhirt@redhat.com>
2022-06-07 14:56:19 -0400

    Start bringing over lower segmentation, kinda.

--- btmesh-driver/src/lower/mod.rs
+++ btmesh-driver/src/lower/mod.rs
@@ -1,14 +1,13 @@
-use crate::Driver;
+mod inbound_segmentation;
+
+use crate::{Driver, DriverError};
 use btmesh_common::Ctl;
 use btmesh_pdu::lower::LowerPDU;
 use btmesh_pdu::network::CleartextNetworkPDU;
 
 impl Driver {
-    fn process_cleartext_network_pdu(&self, network_pdu: &CleartextNetworkPDU<Self>) {
-        let lower_pdu = LowerPDU::parse(network_pdu);
+    fn process_cleartext_network_pdu(&self, network_pdu: &CleartextNetworkPDU<Self>) -> Result<LowerPDU<Self>, DriverError>{
+        Ok(LowerPDU::parse(network_pdu)?)
     }
 
-    fn process_cleartext_access_network_pdu(&self, network_pdu: &CleartextNetworkPDU<Self>) {}
-
-    fn process_cleartext_control_network_pdu(&self, network_pdu: &CleartextNetworkPDU<Self>) {}
 }
