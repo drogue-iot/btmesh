518d49246c129ce1124ee085276d5e663c82ddbd
Bob McWhirter <bmcwhirt@redhat.com>
2022-07-01 11:13:28 -0400

    Push errors down into their own file but rexport at the top.
    Purely for source organization.

---
+++ btmesh-driver/src/error.rs
@@ -0,0 +1,54 @@
+use btmesh_common::mic::InvalidLength;
+use btmesh_common::{InsufficientBuffer, ParseError, SeqRolloverError};
+use btmesh_common::address::InvalidAddress;
+use btmesh_pdu::lower::InvalidBlock;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum DriverError {
+    InvalidState,
+    InvalidKeyLength,
+    CryptoError,
+    InvalidAddress,
+    InsufficientSpace,
+    InvalidKeyHandle,
+    InvalidPDU,
+    ParseError(ParseError),
+    SeqRolloverError,
+}
+
+impl From<InvalidLength> for DriverError {
+    fn from(_: InvalidLength) -> Self {
+        Self::CryptoError
+    }
+}
+
+impl From<SeqRolloverError> for DriverError {
+    fn from(_: SeqRolloverError) -> Self {
+        Self::SeqRolloverError
+    }
+}
+
+impl From<InsufficientBuffer> for DriverError {
+    fn from(_: InsufficientBuffer) -> Self {
+        Self::InsufficientSpace
+    }
+}
+
+impl From<ParseError> for DriverError {
+    fn from(inner: ParseError) -> Self {
+        Self::ParseError(inner)
+    }
+}
+
+impl From<InvalidAddress> for DriverError {
+    fn from(_: InvalidAddress) -> Self {
+        Self::InvalidAddress
+    }
+}
+
+impl From<InvalidBlock> for DriverError {
+    fn from(_: InvalidBlock) -> Self {
+        Self::InvalidState
+    }
+}
--- btmesh-driver/src/lib.rs
+++ btmesh-driver/src/lib.rs
@@ -6,56 +6,8 @@ use btmesh_common::mic::InvalidLength;
 use btmesh_common::{InsufficientBuffer, ParseError, SeqRolloverError};
 use btmesh_pdu::lower::InvalidBlock;
 
+mod error;
 pub mod stack;
-
 pub mod unprovisioned;
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum DriverError {
-    InvalidState,
-    InvalidKeyLength,
-    CryptoError,
-    InvalidAddress,
-    InsufficientSpace,
-    InvalidKeyHandle,
-    InvalidPDU,
-    ParseError(ParseError),
-    SeqRolloverError,
-}
-
-impl From<InvalidLength> for DriverError {
-    fn from(_: InvalidLength) -> Self {
-        Self::CryptoError
-    }
-}
-
-impl From<SeqRolloverError> for DriverError {
-    fn from(_: SeqRolloverError) -> Self {
-        Self::SeqRolloverError
-    }
-}
-
-impl From<InsufficientBuffer> for DriverError {
-    fn from(_: InsufficientBuffer) -> Self {
-        Self::InsufficientSpace
-    }
-}
-
-impl From<ParseError> for DriverError {
-    fn from(inner: ParseError) -> Self {
-        Self::ParseError(inner)
-    }
-}
-
-impl From<InvalidAddress> for DriverError {
-    fn from(_: InvalidAddress) -> Self {
-        Self::InvalidAddress
-    }
-}
-
-impl From<InvalidBlock> for DriverError {
-    fn from(_: InvalidBlock) -> Self {
-        Self::InvalidState
-    }
-}
+pub use error::DriverError;
