f4105d0ac62d6a97d40d8f08697013e6d24dda47
Ulf Lilleengen <lulf@redhat.com>
2022-08-26 13:58:25 +0200

    print it to console

--- btmesh-nrf-softdevice/examples/basic/memusage.sh
+++ btmesh-nrf-softdevice/examples/basic/memusage.sh
@@ -4,4 +4,4 @@ cargo size --release | tail -n 2 > memusage.txt
 echo "Stack" >> memusage.txt
 cargo call-stack --bin basic --format top 2> callstack.error | head -n 10 >> memusage.txt
 
-echo "Done, results are stored in memusage.txt"
+cat memusage.txt
